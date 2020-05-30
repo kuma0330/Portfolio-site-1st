7284fdc5fb6491d294031f6884f69d8f2b8f21b7
wkuma0330 <62786348+wkuma0330@users.noreply.github.com>
2020-05-30 17:51:37 +0900

    スムーズスクロール速度変更

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(function(){
     var href= $(this).attr("href");
     var target = $(href == "#" || href == "" ? 'html' : href); 
     var position = target.offset().top;
-    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    $('body,html').animate({scrollTop:position},{ duration: 'slow', easing: 'swing',});
     return false;
   });
 });
