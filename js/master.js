4afe33581edc6bc9317ed5ba971997261f1f4009
wkuma0330 <62786348+wkuma0330@users.noreply.github.com>
2020-06-05 11:08:27 +0900

    誤字、脱字修正

--- js/master.js
+++ js/master.js
@@ -3,7 +3,7 @@ const work = [
       "title": "お菓子屋の仮想サイト",
       "overview": "お菓子屋の仮想サイトです。",
       "feature-1": "サイト訪問者に印象の残るサイトを目指し、ファーストビューのアニメーションに力を入れました。",
-      "feature-2": "また、こだわりセクションでは、スクロールをトリガーにして時間差でアニメーションをスタートさせるために、GSAPを取り入れました。",
+      "feature-2": "また、こだわりセクションではスクロールをトリガーにして時間差でアニメーションをスタートさせるために、GSAPを取り入れました。",
       "lang": "HTML5/CSS3、jQuery、GSAP",
       "period": "3日",
       "souce-code": "https://github.com/kuma9732/sweets-site",
@@ -14,7 +14,7 @@ const work = [
     {
       "title": "電子部品会社の仮想サイト",
       "overview": "電子部品会社の仮想サイトです。",
-      "feature-1": "作業簡略化を目的に、文章をmaster.jsに一括管理し、サイトへの反映は、URLの数字を割り振ることで、それぞれの製品名、サイズ等を表示させました。",
+      "feature-1": "作業簡略化のため文章をmaster.jsに一括管理し、サイトへの反映はURLの数字を割り振ることでそれぞれの製品名、サイズ等を表示させました。",
       "feature-2": "これにより、製品情報の追加・変更の場合も作業を簡略化することが可能となります。",
       "lang": "HTML5/CSS3、jQuery",
       "period": "7日",
@@ -48,7 +48,7 @@ const work = [
     {
       "title": "電子部品会社の注文内容確認ページ",
       "overview": "商品掲載者はサイト訪問者からの商品注文内容を確認するためのサイトです。",
-      "feature-1": "firebaseのDatabaseに登録された注文内容データを家訓することが出来ます",
+      "feature-1": "firebaseのDatabaseに登録された注文内容データを確認することが出来ます",
       "feature-2": "",
       "lang": "HTML5/CSS3、jQuery、firebase",
       "period": "1日",
